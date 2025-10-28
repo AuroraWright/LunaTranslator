118b69a3c13c369eeadf1a5aff22ae86c27d113e
AuroraWright <AuroraWright@users.noreply.github.com>
2025-10-28 13:21:16 +0100

    Part 2

--- src/cpp/LunaHook/Lang/Lang_host.cpp
+++ src/cpp/LunaHook/Lang/Lang_host.cpp
@@ -2,13 +2,13 @@
 
 std::unordered_map<LANG_STRINGS_HOST, i18nString<wchar_t>>
     _internal_lang_strings_host = {
-        {T_WARNING, L"警告"},
-        {ALREADY_INJECTED, L"已经注入"},
-        {INJECT_FAILED, L"注入失败"},
-        {INVALID_CODEPAGE, L"无法转换文本 (无效的代码页?)"},
-        {PROC_CONN, L"进程 %d 已连接"},
-        {PROC_DISCONN, L"进程 %d 已断开连接"},
-        {UNMATCHABLEVERSION, L"文件版本无法匹配，可能无法正常工作，请重新下载！"},
+    {T_WARNING, L"Warning"},
+    {ALREADY_INJECTED, L"Already injected"},
+    {INJECT_FAILED, L"Injection failed"},
+    {INVALID_CODEPAGE, L"Unable to convert text (invalid code page?)"},
+    {PROC_CONN, L"Process %d is connected"},
+    {PROC_DISCONN, L"Process %d has disconnected."},
+    {UNMATCHABLEVERSION, L"The file version does not match and may not function properly. Please download it again!"},
 };
 
 DEFINEFUNCTION(LANG_STRINGS_HOST, _internal_lang_strings_host, wchar_t, get_host)
